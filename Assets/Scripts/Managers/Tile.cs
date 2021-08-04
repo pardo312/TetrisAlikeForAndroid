9e0161ad2ae1c6173443cc45d472034d6a17c9a9
Daniel <d.pardo@getaclub.io>
2021-08-03 22:36:25 -0500

    Added fall of pieces

--- Assets/Scripts/Managers/Tile.cs
+++ Assets/Scripts/Managers/Tile.cs
@@ -2,8 +2,35 @@
 
 namespace JiufenGames.TetrisAlike.Logic
 {
-    public class Tile: MonoBehaviour
+    public class Tile : MonoBehaviour
     {
-        public bool isFilled;
+        public int _tileRow;
+        public int _tileColumn;
+
+        public bool _isFilled;
+        public bool _isPartFromCurrentPiece;
+
+        private SpriteRenderer _spriteRenderer;
+
+        [HideInInspector] public Color _color;
+
+        public void Awake()
+        {
+            _spriteRenderer = GetComponent<SpriteRenderer>();
+            _color = new Color(0.1f, 0.1f, 0.1f, 1);
+        }
+        public void ChangeColorOfTile(Color newColor)
+        {
+            _color = newColor; 
+            _spriteRenderer.color = _color;
+
+        }
+        public void Reset()
+        {
+            _color = new Color(0.1f, 0.1f, 0.1f, 1);
+            _spriteRenderer.color = _color;
+            _isFilled = false;
+            _isPartFromCurrentPiece = false;
+        }
     }
 }
